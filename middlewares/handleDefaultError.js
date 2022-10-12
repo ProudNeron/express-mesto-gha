70b2183eb3199daee6452569008ddbba2c6e7390
ProudNeron <neronnp4@gmail.com>
2022-10-13 01:15:02 +0700

    fix center error handle

--- middlewares/handleDefaultError.js
+++ middlewares/handleDefaultError.js
@@ -2,6 +2,6 @@ module.exports.handleDefaultError = (err, req, res) => {
   const { statusCode = 500, message } = err;
   res
     .status(err.statusCode)
-    .json
+    .json()
     .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
 };
